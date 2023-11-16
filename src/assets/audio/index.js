84c7a0b7b733c58de0337b391d6f4ea4e91527bd
maru <larrosa.marilina@gmail.com>
2023-11-15 23:19:14 -0300

    ADD audio index, implement more audios

---
+++ src/assets/audio/index.js
@@ -0,0 +1,6 @@
+export {default as nuevemm} from './9mm.mp3'
+export {default as brutoignorante} from './brutoignorante.mp3'
+export {default as ctm} from './ctm.mp3'
+export {default as mevoy} from './mevoy.mp3'
+
+//export {default as} from './.mp3'
--- src/components/ButtonContainer.jsx
+++ src/components/ButtonContainer.jsx
@@ -1,10 +1,14 @@
 import { Button } from './Button'
-import nuevemm from '../assets/audio/9mm.mp3'
+import { nuevemm, brutoignorante, ctm, mevoy } from '../assets/audio'
 
 export const ButtonContainer = () => {
     return (
+
         <div>
             <Button name="9mm" path={nuevemm} />
+            <Button name="brutoignorante" path={brutoignorante} />
+            <Button name="ctm" path={ctm} />
+            <Button name="mevoy" path={mevoy} />
         </div>
     )
 }
