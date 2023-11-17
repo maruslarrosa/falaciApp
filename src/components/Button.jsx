8e5b0a2c6f484cf1010d09599298b5081cf71f01
maru <larrosa.marilina@gmail.com>
2023-11-16 22:32:48 -0300

    ADD Disabled state on button when audio playing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,18 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleAudio } from '../redux/audioReducer';
 
 export const Button = (props) => {
-    const { name, path } = props
-    let audio = new Audio(path)
+  const dispatch = useDispatch();
+  const playing = useSelector((state) => state.audio.playing);
+  const { name, path } = props;
 
-    const initAudio = () => {
-        audio.play()
+  const [audio] = useState(new Audio(path));
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  useEffect(() => {
+    const handleEnded = () => {
+      dispatch(toggleAudio());
+      setIsDisabled(false); // Enable the button when audio ends
+    };
+
+    audio.addEventListener('ended', handleEnded);
+
+    return () => {
+      audio.removeEventListener('ended', handleEnded);
+    };
+  }, [audio, dispatch]);
+
+  const initAudio = () => {
+    dispatch(toggleAudio());
+    setIsDisabled(true); // Disable the button when audio starts playing
+    audio.play();
+  };
+
+  useEffect(() => {
+    if (playing) {
+      setIsDisabled(true); // Disable the button when audio is playing
+    } else {
+      setIsDisabled(false); // Enable the button when audio stops playing
     }
+  }, [playing]);
 
-    return (
-        <button onClick={initAudio}>
-            {name}
-        </button>
-
-
-    )
-}
+  return (
+    <button onClick={initAudio} disabled={isDisabled}>
+      {name}
+    </button>
+  );
+};
